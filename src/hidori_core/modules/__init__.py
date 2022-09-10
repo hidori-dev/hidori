8fa1464fd98b4b4e91d2e7cb3ef934d432c11035
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2022-09-10 06:51:38 +0200

    Add hostname module to hidori_core

--- src/hidori_core/modules/__init__.py
+++ src/hidori_core/modules/__init__.py
@@ -1,4 +1,6 @@
 from hidori_core.modules.base import MODULES_REGISTRY
 from hidori_core.modules.hello import HelloModule
+from hidori_core.modules.hostname import HostnameModule
 
-__all__ = ["MODULES_REGISTRY", "HelloModule"]
+
+__all__ = ["MODULES_REGISTRY", "HelloModule", "HostnameModule"]
---
+++ src/hidori_core/modules/hostname.py
@@ -0,0 +1,33 @@
+from typing import Dict
+
+import dbus
+
+from hidori_core.modules.base import Module
+from hidori_core.schema import Schema
+from hidori_core.utils.messenger import Messenger
+
+
+class HostnameSchema(Schema):
+    name: str
+
+
+class HostnameModule(Module, name="hostname", schema_cls=HostnameSchema):
+    def execute(self, validated_data: Dict[str, str], messenger: Messenger) -> Dict[str, str]:
+        new_hostname = validated_data["name"]
+
+        sysbus = dbus.SystemBus()
+        hostnamed_proxy = sysbus.get_object(
+            "org.freedesktop.hostname1", "/org/freedesktop/hostname1"
+        )
+
+        dbus_iface = dbus.Interface(hostnamed_proxy, "org.freedesktop.DBus.Properties")
+        old_hostname = dbus_iface.Get("org.freedesktop.hostname1", "StaticHostname")
+        if old_hostname == new_hostname:
+            messenger.queue_success(f"hostname already set to {new_hostname}")
+            return {"state": "unaffected"}
+
+        hostname_iface = dbus.Interface(hostnamed_proxy, "org.freedesktop.hostname1")
+        hostname_iface.SetStaticHostname(new_hostname, False)
+
+        messenger.queue_affected(f"hostname changed to {new_hostname}")
+        return {"state": "affected"}
