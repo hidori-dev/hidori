4a42ee3fc03e66b6fc9b657d6573f35ef57389c2
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2022-09-27 23:54:36 +0200

    Add test for a single message output
    Factor out the message_writer into a pytest fixture

--- tests/test_common/test_message_writer.py
+++ tests/test_common/test_message_writer.py
@@ -1,9 +1,29 @@
-from pytest import CaptureFixture
+import freezegun
+import pytest
 
 from hidori_common.cli import CLIMessageWriter
 
 
-def test_print_summary_stub(capsys: CaptureFixture[str]):
-    message_writer = CLIMessageWriter(user="root", target="machine")
+@pytest.fixture(scope="module")
+def message_writer() -> CLIMessageWriter:
+    return CLIMessageWriter(user="root", target="machine")
+
+
+def test_print_summary_stub(
+    capsys: pytest.CaptureFixture[str], message_writer: CLIMessageWriter
+):
     message_writer.print_summary()
     assert capsys.readouterr().out == "\n"
+
+
+@freezegun.freeze_time("23:50:15")
+def test_print_single_message(
+    capsys: pytest.CaptureFixture[str], message_writer: CLIMessageWriter
+):
+    data = {"task": "Hello World", "type": "success", "message": "It worked"}
+
+    message_writer.print_one(data)
+    output = capsys.readouterr().out.splitlines()
+    assert len(output) == 2
+    assert output[0] == "\x1b[1m[root@machine: Hello World]\x1b[0m"
+    assert output[1] == "[23:50:15] \x1b[1m\x1b[32mOK:\x1b[39m\x1b[0m It worked"
