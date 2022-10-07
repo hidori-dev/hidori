97446fac328464d954b8d826059ea13495de2095
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2022-10-07 05:24:24 +0200

    Wrap subprocess call in the run_command function in transport

--- src/hidori_runner/transports/ssh.py
+++ src/hidori_runner/transports/ssh.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
+import json
 import subprocess
 from typing import TYPE_CHECKING
 
 from hidori_common.typings import Transport
+from hidori_runner.transports.errors import TransportError
 
 if TYPE_CHECKING:
     from hidori_runner.drivers import SSHDriver
@@ -17,6 +19,22 @@ SSH_OPTIONS = " ".join(
 )
 
 
+def run_command(popen_cmd: list[str]) -> str:
+    results = subprocess.run(popen_cmd, capture_output=True, text=True)
+    if results.returncode != 0:
+        raise TransportError(
+            json.dumps(
+                {
+                    "type": "error",
+                    "task": "INTERNAL-SSH-TRANSPORT",
+                    "message": results.stderr.strip(),
+                }
+            )
+        )
+
+    return results.stdout
+
+
 class SSHTransport(Transport[SSHDriver]):
     def push(self, source: str, dest: str) -> None:
         ssh_user = self._driver.ssh_user
@@ -28,7 +46,7 @@ class SSHTransport(Transport[SSHDriver]):
             f"{ssh_user}@{ssh_ip}:{dest}".split()
         )
         # TO THE STARS!
-        subprocess.run(cmd)
+        run_command(cmd)
 
     def invoke(self, path: str, args: list[str]) -> str:
         ssh_user = self._driver.ssh_user
@@ -40,6 +58,4 @@ class SSHTransport(Transport[SSHDriver]):
             f"{ssh_user}@{ssh_ip} python3 {path}".split()
         )
         cmd.extend(args)
-        results = subprocess.run(cmd, capture_output=True, text=True)
-        # TODO: raise an exception if something wrong happened
-        return results.stdout
+        return run_command(cmd)
