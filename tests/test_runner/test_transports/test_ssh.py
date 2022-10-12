df3801c24b44a7a095707b0754d00d18d8296558
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2022-10-12 18:45:31 +0200

    Replace separate subprocess mock function with single factory

--- tests/test_runner/test_transports/test_ssh.py
+++ tests/test_runner/test_transports/test_ssh.py
@@ -15,58 +15,16 @@ FAILED_EXEC_MSG = json.dumps(
 )
 
 
-def subproc_scp_ok(*popenargs: str, capture_output: bool, text: bool):
-    return subprocess.CompletedProcess[str](
-        args=popenargs,
-        returncode=0,
-        stdout="",
-        stderr="",
-    )
+def subproc_func_factory(retcode: int, stdout: str = "", stderr: str = ""):
+    def func(*popenargs: str, capture_output: bool, text: bool):
+        return subprocess.CompletedProcess[str](
+            args=popenargs,
+            returncode=retcode,
+            stdout=f"{stdout}\n" if stdout else "",
+            stderr=f"{stderr}\n" if stderr else "",
+        )
 
-
-def subproc_scp_con_closed(*popenargs: str, capture_output: bool, text: bool):
-    return subprocess.CompletedProcess[str](
-        args=popenargs,
-        returncode=255,
-        stdout="",
-        stderr="scp: Connection closed\n",
-    )
-
-
-def subproc_scp_error(*popenargs: str, capture_output: bool, text: bool):
-    return subprocess.CompletedProcess[str](
-        args=popenargs,
-        returncode=1,
-        stdout="",
-        stderr="scp: Some generic error\n",
-    )
-
-
-def subproc_exec_ok(*popenargs: str, capture_output: bool, text: bool):
-    return subprocess.CompletedProcess[str](
-        args=popenargs,
-        returncode=0,
-        stdout=f"{SUCCESS_EXEC_MSG}\n",
-        stderr="",
-    )
-
-
-def subproc_no_exec(*popenargs: str, capture_output: bool, text: bool):
-    return subprocess.CompletedProcess[str](
-        args=popenargs,
-        returncode=2,
-        stdout="python3: can't open file '/foo'\n",
-        stderr="",
-    )
-
-
-def subproc_exec_failed(*popenargs: str, capture_output: bool, text: bool):
-    return subprocess.CompletedProcess[str](
-        args=popenargs,
-        returncode=0,
-        stdout=f"{FAILED_EXEC_MSG}\n",
-        stderr="",
-    )
+    return func
 
 
 @pytest.fixture(scope="module")
@@ -76,7 +34,8 @@ def ssh_transport():
 
 
 def test_transport_push_ok(ssh_transport: SSHTransport):
-    with patch("subprocess.run", side_effect=subproc_scp_ok) as subprocess_run:
+    subproc_mock = subproc_func_factory(retcode=0)
+    with patch("subprocess.run", side_effect=subproc_mock) as subprocess_run:
         ssh_transport.push("/foo/bar", "/bar/foo")
 
     assert subprocess_run.call_count == 1
@@ -100,7 +59,8 @@ def test_transport_push_ok(ssh_transport: SSHTransport):
 
 
 def test_transport_push_con_error(ssh_transport: SSHTransport):
-    with patch("subprocess.run", side_effect=subproc_scp_con_closed) as subprocess_run:
+    subproc_mock = subproc_func_factory(retcode=255, stderr="scp: Connection closed")
+    with patch("subprocess.run", side_effect=subproc_mock) as subprocess_run:
         with pytest.raises(TransportError) as e:
             ssh_transport.push("/foo/bar", "/bar/foo")
 
@@ -113,7 +73,8 @@ def test_transport_push_con_error(ssh_transport: SSHTransport):
 
 
 def test_transport_push_generic_error(ssh_transport: SSHTransport):
-    with patch("subprocess.run", side_effect=subproc_scp_error) as subprocess_run:
+    subproc_mock = subproc_func_factory(retcode=1, stderr="scp: Some generic error")
+    with patch("subprocess.run", side_effect=subproc_mock) as subprocess_run:
         with pytest.raises(TransportError) as e:
             ssh_transport.push("/foo/bar", "/bar/foo")
 
@@ -126,7 +87,8 @@ def test_transport_push_generic_error(ssh_transport: SSHTransport):
 
 
 def test_transport_invoke_executor_ok(ssh_transport: SSHTransport):
-    with patch("subprocess.run", side_effect=subproc_exec_ok) as subprocess_run:
+    subproc_mock = subproc_func_factory(retcode=0, stdout=SUCCESS_EXEC_MSG)
+    with patch("subprocess.run", side_effect=subproc_mock) as subprocess_run:
         result = ssh_transport.invoke("/hidori/executor.py", ["TASK-ID"])
 
     assert result == SUCCESS_EXEC_MSG
@@ -154,7 +116,10 @@ def test_transport_invoke_executor_ok(ssh_transport: SSHTransport):
 
 
 def test_transport_invoke_no_executor_error(ssh_transport: SSHTransport):
-    with patch("subprocess.run", side_effect=subproc_no_exec) as subprocess_run:
+    subproc_mock = subproc_func_factory(
+        retcode=2, stdout="python3: can't open file '/foo'"
+    )
+    with patch("subprocess.run", side_effect=subproc_mock) as subprocess_run:
         with pytest.raises(TransportError) as e:
             ssh_transport.invoke("/foo", [])
 
@@ -167,7 +132,8 @@ def test_transport_invoke_no_executor_error(ssh_transport: SSHTransport):
 
 
 def test_transport_invoke_executor_failed_error(ssh_transport: SSHTransport):
-    with patch("subprocess.run", side_effect=subproc_exec_failed) as subprocess_run:
+    subproc_mock = subproc_func_factory(retcode=0, stdout=FAILED_EXEC_MSG)
+    with patch("subprocess.run", side_effect=subproc_mock) as subprocess_run:
         result = ssh_transport.invoke("/hidori/executor.py", ["TASK-ID"])
 
     assert result == FAILED_EXEC_MSG
