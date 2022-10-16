6153a61b239c7489bee6a5725a7da6968956ddaf
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2022-10-16 21:49:01 +0200

    Export SchemaError in hidori_core.schema

--- src/hidori_core/schema/__init__.py
+++ src/hidori_core/schema/__init__.py
@@ -1,4 +1,5 @@
 from hidori_core.schema.base import Schema
+from hidori_core.schema.errors import SchemaError
 from hidori_core.schema.fields import Dictionary, OneOf, SubSchema, Text
 
-__all__ = ["Dictionary", "Schema", "Text", "OneOf", "SubSchema"]
+__all__ = ["Dictionary", "Schema", "SchemaError", "Text", "OneOf", "SubSchema"]
