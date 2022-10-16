96cad3edf0d87dcc2faaa3ec74353cec23f4d770
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2022-10-16 20:36:37 +0200

    Improve remote executor with additional checks and schema

--- src/hidori_runner/executors/remote.py
+++ src/hidori_runner/executors/remote.py
@@ -2,35 +2,73 @@ import json
 import pathlib
 import sys
 import traceback
+from typing import NoReturn
 
 from hidori_core.modules import MODULES_REGISTRY
+from hidori_core.schema import Schema, SchemaError
 from hidori_core.utils import Messenger
 
 
+class TaskDataSchema(Schema):
+    module: str
+
+
+class TaskSchema(Schema):
+    name: str
+    data: TaskDataSchema
+
+
+def exit_with_error(sys_messenger: Messenger, message: str, code: int = 1) -> NoReturn:
+    sys_messenger.queue_error(message)
+    sys_messenger.flush()
+    assert 0 <= code <= 255
+    raise SystemExit(code)
+
+
 def main() -> None:
+    system_messenger = Messenger("system")
+    if len(sys.argv) != 2:
+        exit_with_error(system_messenger, "internal error - invalid executor args")
+
+    root_path = pathlib.Path(sys.argv[0]).parent
     task_id = sys.argv[1]
-    task_path = pathlib.Path(__file__).parent / f"task-{task_id}.json"
+    task_path = root_path / f"task-{task_id}.json"
+    if not task_path.exists():
+        exit_with_error(
+            system_messenger, "internal error - requested task does not exist"
+        )
+
     with open(task_path) as task_file:
-        data = json.load(task_file)
-
-    # TODO: Add pre-flight env detection and verification
-    # systemd
-    # broaden access and lower reqs
-
-    task_name = data["name"]
-    task_data = data["data"]
-
-    messenger = Messenger(task_name)
-    module = MODULES_REGISTRY[task_data["module"]]
-    module.validate(task_data, messenger)
-    if messenger.is_empty:
         try:
-            module.execute(task_data, messenger)
+            data = json.load(task_file)
+        except json.JSONDecodeError:
+            exit_with_error(
+                system_messenger, "internal error - could not parse task file"
+            )
+
+    try:
+        TaskSchema().validate(data)
+    except SchemaError as e:
+        exit_with_error(
+            system_messenger, f"internal error - invalid task structure: {e}"
+        )
+
+    module = MODULES_REGISTRY.get(data["data"]["module"])
+    if module is None:
+        exit_with_error(
+            system_messenger, "internal error - specified module does not exist"
+        )
+
+    task_messenger = Messenger(data["name"])
+    module.validate(data["data"], task_messenger)
+    if task_messenger.is_empty:
+        try:
+            module.execute(data["data"], task_messenger)
         except Exception as e:
-            messenger.queue_error(
+            task_messenger.queue_error(
                 "".join(traceback.format_exception(type(e), e, e.__traceback__))
             )
-    messenger.flush()
+    task_messenger.flush()
 
 
 if __name__ == "__main__":
