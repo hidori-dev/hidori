5313218b73bdb5180be99f102b5a8ae2667bfff7
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2023-06-26 23:37:21 +0200

    Use Path for pipeline-run command's path to pipeline file

--- src/hidori_cli/commands/pipeline_run.py
+++ src/hidori_cli/commands/pipeline_run.py
@@ -1,3 +1,4 @@
+import pathlib
 from dataclasses import dataclass, field
 
 from hidori_cli.commands.base import BaseData, Command
@@ -6,8 +7,9 @@ from hidori_pipelines import PipelineGroup
 
 @dataclass
 class PipelineRunData(BaseData):
-    # TODO: Filepath type with validation that file exists?
-    pipeline_file: str = field(metadata={"help": "Path to the TOML pipeline file"})
+    pipeline_path: pathlib.Path = field(
+        metadata={"help": "Path to the TOML pipeline file"}
+    )
 
 
 class PipelineRunCommand(Command[PipelineRunData]):
@@ -16,7 +18,7 @@ class PipelineRunCommand(Command[PipelineRunData]):
     data_cls = PipelineRunData
 
     def execute(self, data: PipelineRunData) -> None:
-        group = PipelineGroup.from_toml_path(data.pipeline_file)
+        group = PipelineGroup.from_toml_path(str(data.pipeline_path))
         for pipeline in group:
             pipeline.prepare()
             pipeline.run()
