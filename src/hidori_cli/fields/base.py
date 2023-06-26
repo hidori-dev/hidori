3bd92ad9047a2d931814cfd102d601f351d98bf1
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2023-06-26 17:47:32 +0200

    Add field_type CLI registry and field_name param

--- src/hidori_cli/fields/base.py
+++ src/hidori_cli/fields/base.py
@@ -2,19 +2,31 @@ import argparse
 from typing import Any, Mapping
 
 NATIVE_FIELDS_BY_FIELD_NAME: dict[str, type["Field"]] = {}
+NATIVE_FIELDS_BY_FIELD_TYPE: dict[Any, type["Field"]] = {}
 
 
 class Field:
     action: str | type[argparse.Action]
 
-    def __init_subclass__(cls, field_name: str | None = None) -> None:
+    def __init_subclass__(
+        cls, field_name: str | None = None, field_type: Any = None
+    ) -> None:
+        # Either field_name or field_type must be provided
+        assert (not field_name) ^ (not field_type)
+
         if field_name is not None:
             assert field_name not in NATIVE_FIELDS_BY_FIELD_NAME
             NATIVE_FIELDS_BY_FIELD_NAME[field_name] = cls
+        elif field_type is not None:
+            assert field_name not in NATIVE_FIELDS_BY_FIELD_TYPE
+            NATIVE_FIELDS_BY_FIELD_TYPE[field_type] = cls
 
     @classmethod
     def add_to_parser(
-        cls, parser_obj: argparse.ArgumentParser, field_metadata: Mapping[str, Any]
+        cls,
+        parser_obj: argparse.ArgumentParser,
+        field_name: str,
+        field_metadata: Mapping[str, Any],
     ) -> None:
         ...
 
