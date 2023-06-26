103407931ee4c079d6d81692e31e068c4d211651
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2023-06-26 17:06:23 +0200

    Export field classes and register in the fields init file

---
+++ src/hidori_cli/fields/__init__.py
@@ -0,0 +1,5 @@
+from hidori_cli.fields.base import NATIVE_FIELDS_BY_FIELD_NAME
+from hidori_cli.fields.extra_data import ExtraDataField
+from hidori_cli.fields.version import VersionField
+
+__all__ = ["NATIVE_FIELDS_BY_FIELD_NAME", "ExtraDataField", "VersionField"]
