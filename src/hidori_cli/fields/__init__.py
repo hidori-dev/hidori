12d4fdf6cef3c668992e3d69fc7890c74a3b63b4
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2023-06-26 17:50:58 +0200

    Export filepath field and field type register in fields init

--- src/hidori_cli/fields/__init__.py
+++ src/hidori_cli/fields/__init__.py
@@ -1,5 +1,15 @@
-from hidori_cli.fields.base import NATIVE_FIELDS_BY_FIELD_NAME
+from hidori_cli.fields.base import (
+    NATIVE_FIELDS_BY_FIELD_NAME,
+    NATIVE_FIELDS_BY_FIELD_TYPE,
+)
 from hidori_cli.fields.extra_data import ExtraDataField
+from hidori_cli.fields.filepath import FilePathField
 from hidori_cli.fields.version import VersionField
 
-__all__ = ["NATIVE_FIELDS_BY_FIELD_NAME", "ExtraDataField", "VersionField"]
+__all__ = [
+    "NATIVE_FIELDS_BY_FIELD_NAME",
+    "NATIVE_FIELDS_BY_FIELD_TYPE",
+    "ExtraDataField",
+    "FilePathField",
+    "VersionField",
+]
