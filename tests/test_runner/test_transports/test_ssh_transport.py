32a0b4428a7bd237b4f2ce858e76c8444f1c9655
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2023-06-29 23:25:41 +0200

    Update transport tests for async transport operations

--- tests/test_runner/test_transports/test_ssh_transport.py
+++ tests/test_runner/test_transports/test_ssh_transport.py
@@ -1,7 +1,6 @@
 import json
 import pathlib
-import subprocess
-from unittest.mock import Mock, patch
+from unittest.mock import AsyncMock, Mock, patch
 
 import pytest
 
@@ -9,25 +8,26 @@ from hidori_runner.transports.ssh import SSHTransport
 
 SUCCESS_EXEC_MSG = json.dumps(
     {"type": "success", "task": "Test task", "message": "test task succeeded"}
-)
+).encode()
 FAILED_EXEC_MSG = json.dumps(
     {"type": "error", "task": "Test task", "message": "Traceback ..."}
-)
+).encode()
 FAILED_SYSTEM_MSG = json.dumps(
     {"type": "error", "task": "system", "message": "internal error"}
-)
+).encode()
 
 
-def subproc_func_factory(retcode: int, stdout: str = "", stderr: str = ""):
-    def func(*popenargs: str, capture_output: bool, text: bool):
-        return subprocess.CompletedProcess[str](
-            args=popenargs,
-            returncode=retcode,
-            stdout=f"{stdout}\n" if stdout else "",
-            stderr=f"{stderr}\n" if stderr else "",
-        )
-
-    return func
+def subproc_coro_patch(retcode: int, stdout: bytes = b"", stderr: bytes = b""):
+    stdout = stdout + b"\n"
+    stderr = stderr + b"\n"
+    return patch(
+        "asyncio.create_subprocess_shell",
+        AsyncMock(
+            return_value=Mock(
+                returncode=retcode, communicate=AsyncMock(return_value=(stdout, stderr))
+            )
+        ),
+    )
 
 
 @pytest.fixture(scope="module")
@@ -36,36 +36,25 @@ def ssh_transport():
     return SSHTransport(driver)
 
 
-def test_transport_push_ok(ssh_transport: SSHTransport):
-    subproc_mock = subproc_func_factory(retcode=0)
-    with patch("subprocess.run", side_effect=subproc_mock) as subprocess_run:
-        messages = ssh_transport.push("42", pathlib.Path("/foo/bar"))
+@pytest.mark.asyncio
+async def test_transport_push_ok(ssh_transport: SSHTransport):
+    with subproc_coro_patch(retcode=0) as proc:
+        messages = await ssh_transport.push("42", pathlib.Path("/foo/bar"))
 
     assert messages == []
-    assert subprocess_run.call_count == 1
-    assert subprocess_run.call_args.args == (
-        [
-            "scp",
-            "-o",
-            "ControlMaster=auto",
-            "-o",
-            "ControlPath=~/.ssh/control-%r@%h:%p",
-            "-o",
-            "ControlPersist=yes",
-            "-prq",
-            "-P",
-            "50022",
-            "/foo/bar",
-            "user@127.0.0.1:/tmp/hidori-exchange-42",
-        ],
+    assert proc.call_count == 1
+    assert proc.call_args.args == (
+        "scp -o ControlMaster=auto -o ControlPath=~/.ssh/control-%r@%h:%p "
+        "-o ControlPersist=yes -prq -P 50022 /foo/bar "
+        "user@127.0.0.1:/tmp/hidori-exchange-42",
     )
-    assert subprocess_run.call_args.kwargs == {"capture_output": True, "text": True}
+    assert proc.call_args.kwargs == {"stdout": -1, "stderr": -1}
 
 
-def test_transport_push_con_error(ssh_transport: SSHTransport):
-    subproc_mock = subproc_func_factory(retcode=255, stderr="scp: Connection closed")
-    with patch("subprocess.run", side_effect=subproc_mock) as subprocess_run:
-        messages = ssh_transport.push("42", pathlib.Path("/foo/bar"))
+@pytest.mark.asyncio
+async def test_transport_push_con_error(ssh_transport: SSHTransport):
+    with subproc_coro_patch(retcode=255, stderr=b"scp: Connection closed") as proc:
+        messages = await ssh_transport.push("42", pathlib.Path("/foo/bar"))
 
     assert messages == [
         {
@@ -74,13 +63,13 @@ def test_transport_push_con_error(ssh_transport: SSHTransport):
             "message": "scp: Connection closed",
         }
     ]
-    assert subprocess_run.call_count == 1
+    assert proc.call_count == 1
 
 
-def test_transport_push_generic_error(ssh_transport: SSHTransport):
-    subproc_mock = subproc_func_factory(retcode=1, stderr="scp: Some generic error")
-    with patch("subprocess.run", side_effect=subproc_mock) as subprocess_run:
-        messages = ssh_transport.push("42", pathlib.Path("/foo/bar"))
+@pytest.mark.asyncio
+async def test_transport_push_generic_error(ssh_transport: SSHTransport):
+    with subproc_coro_patch(retcode=1, stderr=b"scp: Some generic error") as proc:
+        messages = await ssh_transport.push("42", pathlib.Path("/foo/bar"))
 
     assert messages == [
         {
@@ -89,43 +78,29 @@ def test_transport_push_generic_error(ssh_transport: SSHTransport):
             "message": "scp: Some generic error",
         }
     ]
-    assert subprocess_run.call_count == 1
+    assert proc.call_count == 1
 
 
-def test_transport_invoke_executor_ok(ssh_transport: SSHTransport):
-    subproc_mock = subproc_func_factory(retcode=0, stdout=SUCCESS_EXEC_MSG)
-    with patch("subprocess.run", side_effect=subproc_mock) as subprocess_run:
-        messages = ssh_transport.invoke("42", "executor.py", ["TASK-ID"])
+@pytest.mark.asyncio
+async def test_transport_invoke_executor_ok(ssh_transport: SSHTransport):
+    with subproc_coro_patch(retcode=0, stdout=SUCCESS_EXEC_MSG) as proc:
+        messages = await ssh_transport.invoke("42", "executor.py", "TASK-ID")
 
     assert messages == [json.loads(SUCCESS_EXEC_MSG)]
-    assert subprocess_run.call_count == 1
-    assert subprocess_run.call_args.args == (
-        [
-            "ssh",
-            "-o",
-            "ControlMaster=auto",
-            "-o",
-            "ControlPath=~/.ssh/control-%r@%h:%p",
-            "-o",
-            "ControlPersist=yes",
-            "-qT",
-            "-p",
-            "50022",
-            "user@127.0.0.1",
-            "python3",
-            "/tmp/hidori-exchange-42/executor.py",
-            "TASK-ID",
-        ],
+    assert proc.call_count == 1
+    assert proc.call_args.args == (
+        "ssh -o ControlMaster=auto -o ControlPath=~/.ssh/control-%r@%h:%p "
+        "-o ControlPersist=yes -qT -p 50022 user@127.0.0.1 "
+        "python3 /tmp/hidori-exchange-42/executor.py TASK-ID",
     )
-    assert subprocess_run.call_args.kwargs == {"capture_output": True, "text": True}
+    assert proc.call_args.kwargs == {"stdout": -1, "stderr": -1}
 
 
-def test_transport_invoke_no_executor_error(ssh_transport: SSHTransport):
-    subproc_mock = subproc_func_factory(
-        retcode=2, stdout="python3: can't open file '/foo'"
-    )
-    with patch("subprocess.run", side_effect=subproc_mock) as subprocess_run:
-        messages = ssh_transport.invoke("42", "/foo", [])
+@pytest.mark.asyncio
+async def test_transport_invoke_no_executor_error(ssh_transport: SSHTransport):
+    expected_stdout = b"python3: can't open file '/foo'"
+    with subproc_coro_patch(retcode=2, stdout=expected_stdout) as proc:
+        messages = await ssh_transport.invoke("42", "/foo", "")
 
     assert messages == [
         {
@@ -134,24 +109,24 @@ def test_transport_invoke_no_executor_error(ssh_transport: SSHTransport):
             "message": "python3: can't open file '/foo'",
         }
     ]
-    assert subprocess_run.call_count == 1
+    assert proc.call_count == 1
 
 
-def test_transport_invoke_executor_failed_exec_error(ssh_transport: SSHTransport):
-    subproc_mock = subproc_func_factory(retcode=0, stdout=FAILED_EXEC_MSG)
-    with patch("subprocess.run", side_effect=subproc_mock) as subprocess_run:
-        messages = ssh_transport.invoke("42", "executor.py", ["TASK-ID"])
+@pytest.mark.asyncio
+async def test_transport_invoke_executor_failed_exec_error(ssh_transport: SSHTransport):
+    with subproc_coro_patch(retcode=0, stdout=FAILED_EXEC_MSG) as proc:
+        messages = await ssh_transport.invoke("42", "executor.py", "TASK-ID")
 
     assert messages == [json.loads(FAILED_EXEC_MSG)]
-    assert subprocess_run.call_count == 1
+    assert proc.call_count == 1
 
 
-def test_transport_invoke_executor_exec_failed_system_error(
+@pytest.mark.asyncio
+async def test_transport_invoke_executor_exec_failed_system_error(
     ssh_transport: SSHTransport,
 ):
-    subproc_mock = subproc_func_factory(retcode=1, stdout=FAILED_SYSTEM_MSG)
-    with patch("subprocess.run", side_effect=subproc_mock) as subprocess_run:
-        messages = ssh_transport.invoke("42", "executor.py", ["TASK-ID"])
+    with subproc_coro_patch(retcode=1, stdout=FAILED_SYSTEM_MSG) as proc:
+        messages = await ssh_transport.invoke("42", "executor.py", "TASK-ID")
 
     assert messages == [json.loads(FAILED_SYSTEM_MSG)]
-    assert subprocess_run.call_count == 1
+    assert proc.call_count == 1
